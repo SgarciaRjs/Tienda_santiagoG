3a8a6ef2dd40c8417b948cab3f129b6ae55a38cd
SgarciaRjs <sgarcia10051@ufide.ac.cr>
2025-09-17 20:38:48 -0600

    Push Semana 1 y 2

---
+++ Tienda/src/main/java/tienda/demo/TiendaSantigoApplication.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaSantigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaSantigoApplication.class, args);
+	}
+
+}
